9662015c4437af5ac1b87ea1636bdd1644eede34
Grace Mignon <grace.mignon@yahoo.co.uk>
2017-10-11 14:11:38 +0700

    update write code once

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -4,6 +4,7 @@ public class CheckingAccount {
     private static final float INTEREST_PERCENTAGE = 0.01f;
     private Money balance = new Money();
     private int transferLimit = 100;
+    private SavingAccount savingacc = new SavingAccount();
 
     public Transfer makeTransfer(String counterAccount, Money amount)
         throws BusinessException {
@@ -12,16 +13,7 @@ public class CheckingAccount {
             throw new BusinessException("Limit exceeded!");
         }
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
+        savingacc.validateAccount();
             return result;
         } else {
             throw new BusinessException("Invalid account number!");
--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -1,6 +1,6 @@
 package eu.sig.training.ch04;
 
-// tag::SavingsAccount[]
+
 public class SavingsAccount {
     private static final float INTEREST_PERCENTAGE = 0.04f;
     private Money balance = new Money();
@@ -8,17 +8,7 @@ public class SavingsAccount {
 
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 2. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
+    	validateAccount();
             // 3. Check whether withdrawal is to registered counter account:
             if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
             {
@@ -39,5 +29,19 @@ public class SavingsAccount {
             balance.substract(interest);
         }
     }
+    
+    public void validateAccount() {
+    	
+    	 // 1. Assuming result is 9-digit bank account number, validate 11-test:
+    	 int sum = 0; // <1>
+         for (int i = 0; i < counterAccount.length(); i++) {
+             char character = counterAccount.charAt(i);
+             int characterValue = Character.getNumericValue(character);
+             sum = sum + (9 - i) * characterValue;
+         }
+         if (sum % 11 == 0) {
+             // 2. Look up counter account and make transfer object:
+             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+             Transfer result = new Transfer(this, acct, amount); // <2>
+    }
 }
-// end::SavingsAccount[]
